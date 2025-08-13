306ef2957e4fe349f3446d04306341b9b356b4e3
Issa Rice <riceissa@gmail.com>
2025-08-12 20:38:21 -0700

    reorder shit

--- lox/interpreter.go
+++ lox/interpreter.go
@@ -16,89 +16,6 @@ func interpret(expression Expr) {
 	}
 }
 
-func stringify(object any) string {
-	if object == nil {
-		return "nil"
-	}
-	switch object.(type) {
-	case float64:
-		text := fmt.Sprintf("%v", object)
-		if strings.HasSuffix(text, ".0") {
-			text = text[0 : len(text)-2]
-		}
-		return text
-	default:
-		return fmt.Sprintf("%v", object)
-	}
-}
-
-func interpret_literal_expr(expr Literal) any {
-	return expr.value
-}
-
-func interpret_unary_expr(expr Unary) any {
-	right, _ := evaluate(expr.right)
-
-	switch expr.operator.token_type {
-	case BANG:
-		return !isTruthy(right)
-	case MINUS:
-		checkNumberOperand(expr.operator, right)
-		return -(right.(float64))
-	}
-
-	// Unreachable
-	return nil
-}
-
-func checkNumberOperand(operator Token, operand any) error {
-	switch operand.(type) {
-	case float64:
-		return nil
-	default:
-		return errors.New(fmt.Sprintf("Operand %v must be a number.", operand))
-	}
-}
-
-func checkNumberOperands(operator Token, left any, right any) error {
-	_, leftIsFloat := left.(float64)
-	_, rightIsFloat := right.(float64)
-	if leftIsFloat && rightIsFloat {
-		return nil
-	}
-	return errors.New(fmt.Sprintf("Operands %v must be numbers.", operator))
-}
-
-func isTruthy(object any) bool {
-	if object == nil {
-		return false
-	}
-	switch object.(type) {
-	case bool:
-		return object.(bool)
-	default:
-		return true
-	}
-}
-
-func isEqual(a any, b any) bool {
-	if a == nil && b == nil {
-		return true
-	}
-	if a == nil {
-		return false
-	}
-	// TODO: might need to use something like reflect.DeepEqual
-	// or whatever; I don't know if Go's native == operator
-	// is what we want here.
-	return a == b
-}
-
-func interpret_grouping_expr(expr Grouping) any {
-	result, _ := evaluate(expr.expression)
-	return result
-}
-
 func evaluate(expr Expr) (any, error) {
 	switch v := expr.(type) {
 	case Binary:
@@ -162,4 +79,87 @@ func interpret_binary_expr(expr Binary) any {
 
 	// Unreachable.
 	return nil
+}
+
+func interpret_grouping_expr(expr Grouping) any {
+	result, _ := evaluate(expr.expression)
+	return result
+}
+
+func interpret_literal_expr(expr Literal) any {
+	return expr.value
+}
+
+func interpret_unary_expr(expr Unary) any {
+	right, _ := evaluate(expr.right)
+
+	switch expr.operator.token_type {
+	case BANG:
+		return !isTruthy(right)
+	case MINUS:
+		checkNumberOperand(expr.operator, right)
+		return -(right.(float64))
+	}
+
+	// Unreachable
+	return nil
+}
+
+func stringify(object any) string {
+	if object == nil {
+		return "nil"
+	}
+	switch object.(type) {
+	case float64:
+		text := fmt.Sprintf("%v", object)
+		if strings.HasSuffix(text, ".0") {
+			text = text[0 : len(text)-2]
+		}
+		return text
+	default:
+		return fmt.Sprintf("%v", object)
+	}
+}
+
+func checkNumberOperand(operator Token, operand any) error {
+	switch operand.(type) {
+	case float64:
+		return nil
+	default:
+		return errors.New(fmt.Sprintf("Operand %v must be a number.", operand))
+	}
+}
+
+func checkNumberOperands(operator Token, left any, right any) error {
+	_, leftIsFloat := left.(float64)
+	_, rightIsFloat := right.(float64)
+	if leftIsFloat && rightIsFloat {
+		return nil
+	}
+	return errors.New(fmt.Sprintf("Operands %v must be numbers.", operator))
+}
+
+func isTruthy(object any) bool {
+	if object == nil {
+		return false
+	}
+	switch object.(type) {
+	case bool:
+		return object.(bool)
+	default:
+		return true
+	}
+}
+
+func isEqual(a any, b any) bool {
+	if a == nil && b == nil {
+		return true
+	}
+	if a == nil {
+		return false
+	}
+	// TODO: might need to use something like reflect.DeepEqual
+	// or whatever; I don't know if Go's native == operator
+	// is what we want here.
+	return a == b
 }
