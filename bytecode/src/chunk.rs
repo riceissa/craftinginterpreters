10970e5b254daa18779d3991864e11cc96757bd9
Issa Rice <riceissa@gmail.com>
2025-09-04 13:14:54 -0700

    I might regret this, but we are going RAII

--- bytecode/src/chunk.rs
+++ bytecode/src/chunk.rs
@@ -15,29 +15,32 @@ pub struct DynamicArray<T> {
     pub values: *mut T,
 }
 
+impl<T> Drop for DynamicArray<T> {
+    fn drop(&mut self) {
+        free_array::<T>(self.values, self.capacity);
+    }
+}
+
 impl<T> DynamicArray<T> {
-    pub fn init(array: &mut DynamicArray<T>) {
-        array.count = 0;
-        array.capacity = 0;
-        array.values = ptr::null::<T>() as *mut T;
+    pub fn new() -> Self {
+        DynamicArray::<T>{
+            count: 0,
+            capacity: 0,
+            values: ptr::null::<T>() as *mut T,
+        }
     }
 
-    pub fn free(array: &mut DynamicArray<T>) {
-        free_array::<T>(array.values, array.capacity);
-        Self::init(array);
-    }
-
-    pub fn write(array: &mut DynamicArray<T>, value: T) {
-        if array.capacity < array.count + 1 {
-            let old_capacity = array.capacity;
-            array.capacity = grow_capacity(old_capacity);
-            array.values = grow_array::<T>(array.values, old_capacity, array.capacity);
+    pub fn write(&mut self, value: T) {
+        if self.capacity < self.count + 1 {
+            let old_capacity = self.capacity;
+            self.capacity = grow_capacity(old_capacity);
+            self.values = grow_array::<T>(self.values, old_capacity, self.capacity);
         }
 
         unsafe {
-            *array.values.add(array.count as usize) = value;
+            *self.values.add(self.count as usize) = value;
         }
-        array.count += 1;
+        self.count += 1;
     }
 }
 
@@ -48,25 +51,21 @@ pub struct Chunk {
 }
 
 impl Chunk {
-    pub fn init(chunk: &mut Chunk) {
-        DynamicArray::<u8>::init(&mut chunk.chunk);
-        DynamicArray::<i32>::init(&mut chunk.lines);
-        ValueArray::init(&mut chunk.constants);
+    pub fn new() -> Self {
+        Chunk {
+            chunk: DynamicArray::<u8>::new(),
+            lines: DynamicArray::<i32>::new(),
+            constants: ValueArray::new(),
+        }
     }
 
-    pub fn free(chunk: &mut Chunk) {
-        DynamicArray::<u8>::free(&mut chunk.chunk);
-        DynamicArray::<i32>::free(&mut chunk.lines);
-        ValueArray::free(&mut chunk.constants);
+    pub fn add_constant(&mut self, value: Value) -> isize {
+        self.constants.write(value);
+        return self.constants.count - 1;
     }
 
-    pub fn add_constant(chunk: &mut Chunk, value: Value) -> isize {
-        ValueArray::write(&mut chunk.constants, value);
-        return chunk.constants.count - 1;
-    }
-
-    pub fn write(chunk: &mut Chunk, byte: u8, line: i32) {
-        DynamicArray::<u8>::write(&mut chunk.chunk, byte);
-        DynamicArray::<i32>::write(&mut chunk.lines, line);
+    pub fn write(&mut self, byte: u8, line: i32) {
+        self.chunk.write(byte);
+        self.lines.write(line);
     }
 }
--- bytecode/src/main.rs
+++ bytecode/src/main.rs
@@ -3,36 +3,18 @@ mod debug;
 mod memory;
 mod value;
 
-use crate::chunk::{Chunk, OpCode, DynamicArray};
+use crate::chunk::{Chunk, OpCode};
 use crate::debug::disassemble_chunk;
-use crate::value::{ValueArray, Value};
+use crate::value::{Value};
 
 fn main() {
-    let mut chunk: Chunk = Chunk {
-        chunk: DynamicArray::<u8>{
-            count: 0,
-            capacity: 0,
-            values: 0 as *mut u8,
-        },
-        lines: DynamicArray::<i32>{
-            count: 0,
-            capacity: 0,
-            values: 0 as *mut i32,
-        },
-        constants: ValueArray{
-            count: 0,
-            capacity: 0,
-            values: 0 as *mut Value,
-        },
-    };
-    Chunk::init(&mut chunk);
+    let mut chunk: Chunk = Chunk::new();
 
-    let constant = Chunk::add_constant(&mut chunk, Value(1.2));
-    Chunk::write(&mut chunk, OpCode::Constant as u8, 123);
-    Chunk::write(&mut chunk, constant as u8, 123);
+    let constant = chunk.add_constant(Value(1.2));
+    chunk.write(OpCode::Constant as u8, 123);
+    chunk.write(constant as u8, 123);
 
-    Chunk::write(&mut chunk, OpCode::Return as u8, 123);
+    chunk.write(OpCode::Return as u8, 123);
 
     disassemble_chunk(&mut chunk, "test chunk");
-    Chunk::free(&mut chunk);
 }
