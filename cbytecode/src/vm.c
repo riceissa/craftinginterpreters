ee71fcbfd8c971f6d45d6970152180b97f5793bf
Issa Rice <riceissa@gmail.com>
2025-09-06 18:39:59 -0700

    finish the chapter

--- cbytecode/src/vm.c
+++ cbytecode/src/vm.c
@@ -1,35 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
+#include "debug.h"
 #include "vm.h"
 
 VM vm;
 
+static void reset_stack()
+{
+    vm.stack_top = vm.stack;
+}
+
 void init_vm()
 {
+    reset_stack();
 }
 
 void free_vm()
 {
 }
 
+void push(Value value)
+{
+    // The book didn't check for this... perhaps he'll do the check somewhere
+    // else.
+    if (vm.stack_top - vm.stack >= STACK_MAX) {
+        printf("Stack overflow");
+        exit(1);
+    }
+
+    *vm.stack_top = value;
+    vm.stack_top++;
+}
+
+Value pop()
+{
+    // Just like the push, the book didn't really check for bad situations...
+    // I'm guessing he'll do something in a few chapters, hopefully!
+    if (vm.stack_top == &vm.stack[0]) {
+        printf("Trying to pop from an empty stack!");
+        exit(1);
+    }
+
+    vm.stack_top--;
+    return *vm.stack_top;
+}
+
 static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
     for (;;)
     {
-        uint8_t instruction;
-        switch (instruction = READ_BYTE())
+#ifdef DEBUG_TRACE_EXECUTION
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stack_top; slot++)
         {
+            printf("[ ");
+            print_value(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+        disassemble_instruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+#endif
+        uint8_t instruction;
+        switch (instruction = READ_BYTE()) {
         case OP_CONSTANT: {
             Value constant = READ_CONSTANT();
-            print_value(constant);
-            printf("\n");
-            break;
-        }
+            push(constant);
+        } break;
+        case OP_ADD: {
+            BINARY_OP(+);
+        } break;
+        case OP_SUBTRACT: {
+            BINARY_OP(-);
+        } break;
+        case OP_MULTIPLY: {
+            BINARY_OP(*);
+        } break;
+        case OP_DIVIDE: {
+            BINARY_OP(/);
+        } break;
+        case OP_NEGATE: {
+            push(-pop());
+        } break;
         case OP_RETURN: {
+            print_value(pop());
+            printf("\n");
             return INTERPRET_OK;
         }
         }
@@ -37,6 +102,7 @@ static InterpretResult run()
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk *chunk)
