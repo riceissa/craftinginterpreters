2009487b424bb26f5b9a541a91041f9f9bb5c6fa
Issa Rice <riceissa@gmail.com>
2025-09-06 21:10:53 -0700

    align

--- cbytecode/src/vm.c
+++ cbytecode/src/vm.c
@@ -51,11 +51,11 @@ static InterpretResult run()
 {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-#define BINARY_OP(op) \
-    do { \
+#define BINARY_OP(op)     \
+    do {                  \
         double b = pop(); \
         double a = pop(); \
-        push(a op b); \
+        push(a op b);     \
     } while (false)
 
     for (;;)
