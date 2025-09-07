5100858f52d63378e65fef4761e632b386c08b85
Issa Rice <riceissa@gmail.com>
2025-09-07 15:00:25 -0700

    finish chapter 16

--- cbytecode/src/vm.c
+++ cbytecode/src/vm.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "vm.h"
 
@@ -105,9 +106,8 @@ static InterpretResult run()
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk *chunk)
+InterpretResult interpret(const char *source)
 {
-    vm.chunk = chunk;
-    vm.ip = vm.chunk->code;
-    return run();
+    compile(source);
+    return INTERPRET_OK;
 }
